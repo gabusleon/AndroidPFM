04110098f9b9846e6e8b828da0932f7e14a491b9
macintosh <carlos.iniguez79@gmail.com>
2013-05-01 18:53:04 +0200

    Agregacion de control para obtener un Array o un solo objeto dentro de una Array.

--- AndroidPFM/src/pfm/android/jpa/JPAFacturaDAO.java
+++ AndroidPFM/src/pfm/android/jpa/JPAFacturaDAO.java
@@ -9,6 +9,8 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import android.util.Log;
 import pfm.android.dao.FacturaDAO;
 import pfm.entidades.Factura;
 import pfm.entidades.rest.ItemCarro;
@@ -34,22 +36,28 @@ public class JPAFacturaDAO extends JPAGenericDAO<Factura, Integer> implements Fa
 				HttpResponse resp = httpClient.execute(del);
 				String respStr = EntityUtils.toString(resp.getEntity());
 				JSONObject respJSON = new JSONObject(respStr);
-				JSONArray detallesJSON = respJSON.getJSONArray("itemProducto");
 
-				for (int i = 0; i < detallesJSON.length(); i++) {
-					JSONObject productoJSON = detallesJSON.getJSONObject(i);
-					ItemProducto p = new ItemProducto(productoJSON.getInt("idBodegaDetalle"), 
-							productoJSON.getString("nombreProducto"),
-							productoJSON.getDouble("subtotal"), 
-							productoJSON.getInt("cantidad"), 
-							productoJSON.getDouble("precio"),
-							productoJSON.getInt("idFacturaDetalle"));
+				if (respStr.startsWith("{")) {
+					JSONObject pJSON = respJSON.getJSONObject("itemProducto");
+					ItemProducto p = new ItemProducto(pJSON.getInt("idBodegaDetalle"), pJSON.getString("nombreProducto"), pJSON.getDouble("subtotal"),
+							pJSON.getInt("cantidad"), pJSON.getDouble("precio"), pJSON.getInt("idFacturaDetalle"));
 					listaProductos.add(p);
 
+				} else {
+
+					JSONArray detallesJSON = respJSON.getJSONArray("itemProducto");
+					for (int i = 0; i < detallesJSON.length(); i++) {
+						JSONObject productoJSON = detallesJSON.getJSONObject(i);
+						ItemProducto p = new ItemProducto(productoJSON.getInt("idBodegaDetalle"), productoJSON.getString("nombreProducto"),
+								productoJSON.getDouble("subtotal"), productoJSON.getInt("cantidad"), productoJSON.getDouble("precio"),
+								productoJSON.getInt("idFacturaDetalle"));
+						listaProductos.add(p);
+					}
 				}
 
 				return listaProductos;
 			} catch (Exception ex) {
+				Log.i("CINIGUEZ", " EXCEPTION : " + ex.getMessage().toString());
 				return null;
 			}
 		}
@@ -57,8 +65,8 @@ public class JPAFacturaDAO extends JPAGenericDAO<Factura, Integer> implements Fa
 
 	@Override
 	public List<ItemCarro> getListaCarros(int idUsuario, int idAgencia) {
-		List<ItemCarro> listaCarros= new ArrayList<ItemCarro>();
-		
+		List<ItemCarro> listaCarros = new ArrayList<ItemCarro>();
+
 		HttpClient httpClient = new DefaultHttpClient();
 		HttpGet del = new HttpGet(this.uri + this.urlREST + "/carros/" + idUsuario + "/" + idAgencia);
 		del.setHeader("content-type", "application/json");
@@ -68,16 +76,27 @@ public class JPAFacturaDAO extends JPAGenericDAO<Factura, Integer> implements Fa
 			String respStr = EntityUtils.toString(resp.getEntity());
 
 			JSONObject respJSON = new JSONObject(respStr);
-			JSONArray carrosJSON = respJSON.getJSONArray("carroCompras");
-			
-			for (int i = 0; i < carrosJSON.length(); i++) {
-				JSONObject carroJSON = carrosJSON.getJSONObject(i);
+			if (respStr.startsWith("{")) {
+				JSONObject pJSON = respJSON.getJSONObject("itemProducto");
 				ItemCarro c = new ItemCarro();
-				c.setFechaCreacion(carroJSON.getString("fechaCreacion"));
-				c.setIdFactura(carroJSON.getInt("idFactura"));
-				c.setNombreAgencia(carroJSON.getString("nombreAgencia"));
-				c.setTotal(carroJSON.getDouble("total"));
+				c.setFechaCreacion(pJSON.getString("fechaCreacion"));
+				c.setIdFactura(pJSON.getInt("idFactura"));
+				c.setNombreAgencia(pJSON.getString("nombreAgencia"));
+				c.setTotal(pJSON.getDouble("total"));
 				listaCarros.add(c);
+
+			} else {
+				JSONArray carrosJSON = respJSON.getJSONArray("carroCompras");
+
+				for (int i = 0; i < carrosJSON.length(); i++) {
+					JSONObject carroJSON = carrosJSON.getJSONObject(i);
+					ItemCarro c = new ItemCarro();
+					c.setFechaCreacion(carroJSON.getString("fechaCreacion"));
+					c.setIdFactura(carroJSON.getInt("idFactura"));
+					c.setNombreAgencia(carroJSON.getString("nombreAgencia"));
+					c.setTotal(carroJSON.getDouble("total"));
+					listaCarros.add(c);
+				}
 			}
 			return listaCarros;
 		} catch (Exception ex) {
